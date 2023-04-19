9c0bf01f15869f1ee6a61a71c3822c28ddf5292e
AntonPetrenko <109439896+mantr88@users.noreply.github.com>
2023-04-19 22:51:45 +0300

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,28 @@
-import { RecipeList } from "./RecipeList";
-import recipes from '../recipes.json'
+import { Component } from 'react';
+import { RecipeList } from './RecipeList/RecipeList';
+import initialRecepies from '../recipes.json';
+import { GlobalStyle } from './GlobalStyle';
+import { Layout } from './Layout/Layout';
 
-export const App = () => {
-  return (
-    <>
-      <RecipeList items={recipes}/>
-    </>
-  );
-};
+export class App extends Component {
+  state = {
+    recipes: initialRecepies,
+  };
+
+  deleteRecipe = recipeId => {
+    this.setState(pervState => ({
+      recipes: pervState.recipes.filter(recepie => recepie.id !== recipeId),
+    }));
+  };
+
+  render() {
+    return (
+      <Layout>
+        <div>Recipe Form</div>
+        <RecipeList items={this.state.recipes} onDelite={this.deleteRecipe} />
+        <div>Image modal</div>
+        <GlobalStyle />
+      </Layout>
+    );
+  }
+}
---
+++ src/components/RecipeList/RecipeList.js
@@ -0,0 +1,23 @@
+import { RecipeCard } from '../RecipeCard/RecipeCard';
+import PropTypes from 'prop-types';
+import { List } from './RecipeList.styled';
+
+export const RecipeList = ({ items, onDelite }) => {
+  return (
+    <List>
+      {items.map(item => (
+        <li key={item.id}>
+          <RecipeCard item={item} onDelite={onDelite} />
+        </li>
+      ))}
+    </List>
+  );
+};
+
+RecipeList.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
